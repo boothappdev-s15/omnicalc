277bcfc8d592deb1ba19a3dad11ce6ce81627eb3
mpoelst <mpoelstr@chicagobooth.edu>
2015-04-28 18:40:22 -0500

    homework 4 start

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,8 @@ class CalculationsController < ApplicationController
   def word_count
     @text = params[:user_text]
     @special_word = params[:user_word]
-
+    # @text = "My name is Megan"
+    # @special_word = "Megan"
     # ================================================================================
     # Your code goes below.
     # The text the user input is in the string @text.
@@ -11,14 +12,26 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.size
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.size - @text.count(" ")
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split(" ").count
 
-    @occurrences = "Replace this string with your answer."
-  end
+
+@occurrences = "Replace this string with your answer."
+
+
+
+    my_words = @text.split(" ")
+    matched_words = []
+    my_words.each do |word|
+        if word = @special_word
+           matched_words.push
+           # use @word_count to count the # of special words?
+           end
+        end
+    end
 
   def loan_payment
     @apr = params[:annual_percentage_rate].to_f
@@ -32,7 +45,15 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+
+    @monthly_payment =""
+        #apr_decimal = @apr/100/12
+        #num = apr_decimal * @principal
+        #months = @years / -12
+        #den = 1 - (1+apr_decimal) ** months
+        #@loan_payment = num / den
+
+
   end
 
   def time_between
@@ -48,12 +69,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @weeks/52
   end
 
   def descriptive_statistics
@@ -64,25 +85,30 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    @median = @numbers.length/2
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum/@count
 
-    @variance = "Replace this string with your answer."
+    new_numbers = []
+    @numbers.each do |number|
+        new_numbers.push((number - @mean)**2)
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance = new_numbers.sum/new_numbers.count
+
+    #@standard_deviation = @variance.sqrt
 
     @mode = "Replace this string with your answer."
   end
